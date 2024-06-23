74af124da4dd3f2be5f392a254321fee4de73dfe
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2024-06-23 15:32:56 -0300

    CRUD Clientes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ sales = {}
 
 clients = {}
 
-#------------------------------------------------------------
+#------------------------------------------------------------#
 
 module = " "
 
@@ -31,6 +31,7 @@ while module != "0":
     """)
     module = input("Escolha sua opção\n->")
 
+#---------------------------------------------------------------------------#
    
     if module == "1":
         
@@ -60,7 +61,7 @@ while module != "0":
                 print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
                 
                 print("--" * 10)
-                name = str(input(" Nome: "))
+                product_name = str(input(" Nome: "))
                 print()
                 code = str(input(" Código: "))
                 print()
@@ -70,7 +71,7 @@ while module != "0":
                 print()
                 price = float(input(" Preço: "))
                 print()
-                products[code] = [name, fabrication, validity, price]
+                products[code] = [product_name, fabrication, validity, price]
                 print(products)
                 print("--" * 10)
                 print()
@@ -91,8 +92,11 @@ while module != "0":
                 if code in products:
                     print("--" * 10)
                     print("Nome: ", products[code][0])
+                    print()
                     print("Data de Fabricação: ", products[code][1])
+                    print()
                     print("Data de Validade: ", products[code][2])
+                    print()
                     print("Preço: R$ ", products[code][3])
                     print("--" * 10)
                 else:
@@ -114,12 +118,15 @@ while module != "0":
                     print("--" * 10)
                     print("Insira os novos dados:")
                     print()
-                    name = input("Nome: ")
+                    product_name = input("Nome: ")
+                    print()
                     fabrication = input("Data de Fabricação: ")
+                    print()
                     validity = input("Data de Validade:")
+                    print()
                     price = input("Preço: R$ ")
                     print("--" * 10)
-                    products[code] = [name, fabrication, validity, price]
+                    products[code] = [product_name, fabrication, validity, price]
                     print(products)
                     print()
                 else:
@@ -218,66 +225,116 @@ while module != "0":
         
         
         
-        input("Pressione <ENTER> para continuar.")
 #-------------------------------------------------------------------------#    
    
     elif module == "3":
-        os.system('cls || clear')
-        print("""
+        
+        module2 = " "
+        
+        while module2 != "0":
+            os.system('cls || clear')
+            print("""
     ______________________________________________________________
     |=-=-=-=-=-=-=-> Você está no Módulo Clientes <-=-=-=-=-=-=-=|
     |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
-    |=-=-=-=-=->    1 - Cadastrar Cliente             <-=-=-=-=-=|
-    |=-=-=-=-=->    2 - Consultar Cliente             <-=-=-=-=-=|
-    |=-=-=-=-=->    3 - Alterar Cliente               <-=-=-=-=-=|
-    |=-=-=-=-=->    4 - Excluir Cliente               <-=-=-=-=-=|
-    |=-=-=-=-=->    0 - Retornar ao Menu Principal    <-=-=-=-=-=|
+    |=-=-=-=-=->     1 - Cadastrar Cliente            <-=-=-=-=-=|
+    |=-=-=-=-=->     2 - Exibir Cliente               <-=-=-=-=-=|
+    |=-=-=-=-=->     3 - Alterar Cliente              <-=-=-=-=-=|
+    |=-=-=-=-=->     4 - Excluir Cliente              <-=-=-=-=-=|
+    |=-=-=-=-=->     0 - Retornar ao Menu Principal   <-=-=-=-=-=|
     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
     """)
-        module2 = input("Escolha sua opção\n->")
+            module2 = input("Escolha sua opção\n->")
 
-        if module2 == "1":
-            os.system('cls || clear')
+            if module2 == "1":
+                os.system('cls || clear')
 
-            print("___________________________________________")
-            print("|=-=-=-=-=-> Cadastrar Cliente <-=-=-=-=-=|")
-            print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+                print("___________________________________________")
+                print("|=-=-=-=-=-> Cadastrar Cliente <-=-=-=-=-=|")
+                print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
 
+                print("--" * 10)
+                client_name = str(input("Nome: "))
+                print()
+                cpf = str(input("Cpf: "))
+                print()
+                phone_number = str(input("Telefone: "))
+                print()
+                clients[cpf] = [client_name, phone_number]
+                print(clients)
+                print("--" * 10)
+                print()
+                print("Cliente Cadastrado com Sucesso!")
+                print()
+                input("Pressione <ENTER> para continuar.")
+
+            elif module2 == "2":
+                os.system('cls || clear')
+
+                print("________________________________________")
+                print("|=-=-=-=-=-> Exibir Cliente <-=-=-=-=-=|")
+                print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+
+                cpf = str(input("Digite o Cpf do Cliente: "))
+
+                if cpf in clients:
+                    print("--" * 10)
+                    print("Nome: ", clients[cpf][0])
+                    print()
+                    print("Telefone: ", clients[cpf][1])
+                    print("--" * 10)
+                
+                else:
+                    print("Cliente Inexistente!")
+                
+                input("Pressione <ENTER> para continuar.")
+
+
+
+            elif module2 == "3":
+                os.system('cls || clear')
+
+                print("_________________________________________")
+                print("|=-=-=-=-=-> Alterar Cliente <-=-=-=-=-=|")
+                print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+
+                cpf = str(input("Insira o Cpf do Cliente: "))
+
+                if cpf in clients:
+                    print("--" * 10)
+                    print("Insira os novos dados:")
+                    print()
+                    client_name = str(input("Nome: "))
+                    print()
+                    phone_number = str(input("Telefone: "))
+                    print("--" * 10)
+                    clients[cpf] = [client_name, phone_number]
+                    print(clients)
+                    print()
+                else:
+                    print("Cliente Inexistente")
+
+                print("Cliente Alterado com Sucesso!")
+                print()
+                input("Pressione <ENTER> para continuar.")
+
+            elif module2 == "4":
+                os.system('cls || clear')
+
+                print("_________________________________________")
+                print("|=-=-=-=-=-> Excluir Cliente <-=-=-=-=-=|")
+                print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
         
+                cpf = input("Insira o Cpf do Cliente: ")
 
-        elif module2 == "2":
-            os.system('cls || clear')
-
-            print("___________________________________________")
-            print("|=-=-=-=-=-> Consultar Cliente <-=-=-=-=-=|")
-            print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-
-
-
-        elif module2 == "3":
-            os.system('cls || clear')
-
-            print("_________________________________________")
-            print("|=-=-=-=-=-> Alterar Cliente <-=-=-=-=-=|")
-            print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-
-
-
-        elif module2 == "4":
-            os.system('cls || clear')
-
-            print("___________________________________________")
-            print("|=-=-=-=-=-> Excluir Cliente <-=-=-=-=-=|")
-            print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-        
-        
-        
-        
-        
-        
-        
-        
-        input("Pressione <ENTER> para continuar.")
+                if cpf in clients:
+                    del clients[cpf]
+                else:
+                    print("Cliente Inexistente!")
+                
+                print("Cliente Excluído com Sucesso!")
+                print()
+                input("Pressione <ENTER> para continuar.")        
 #-------------------------------------------------------------------------#    
     elif module == "4":
         os.system('cls || clear')
